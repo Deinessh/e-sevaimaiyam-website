e0cc4c6718b9ef54d3f1087885fde7fb242862f7
Deinessh <deinessh@users.noreply.github.com>
2026-07-23 18:28:48 +0530

    blog

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         blog: resolve(__dirname, 'blog.html'),
-        contact: resolve(__dirname, 'contact.html')
+        contact: resolve(__dirname, 'contact.html'),
+        post: resolve(__dirname, 'post.html')
       }
     }
   }
